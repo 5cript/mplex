f83ad43f8daf7d23f3e0abe10be402d82770e354
5cript <Tim06TR@gmail.com>
2015-06-17 23:07:18 +0200

    simple if construct added

---
+++ if.hpp
@@ -0,0 +1,22 @@
+#ifndef IF_HPP_INCLUDED
+#define IF_HPP_INCLUDED
+
+namespace mplex {
+    template <bool Condition,
+              typename TrueT,
+              typename FalseT>
+    struct if_ {
+        using type = TrueT;
+    };
+
+    template <typename TrueT,
+              typename FalseT>
+    struct if_ <false, TrueT, FalseT> {
+        using type = FalseT;
+    };
+
+    template <bool Condition, typename TrueT, typename FalseT>
+    using if_t = typename if_<Condition, TrueT, FalseT>::type;
+}
+
+#endif // IF_HPP_INCLUDED
