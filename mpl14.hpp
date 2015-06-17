e2b594c21f0f4935ebcc61e258cd3f8018d71424
5cript <Tim06TR@gmail.com>
2015-06-18 00:59:00 +0200

    added all the new headers

--- mpl14.hpp
+++ mpl14.hpp
@@ -1,12 +1,26 @@
 #ifndef MPL14_HPP_INCLUDED
 #define MPL14_HPP_INCLUDED
 
-#include "apply.hpp"
-#include "front.hpp"
-#include "back.hpp"
-#include "push_back.hpp"
-#include "push_front.hpp"
-#include "pop_back.hpp"
-#include "pop_front.hpp"
+// this file is for convenience, but actually I personally do not recommend to use it
+
+// sequences / tuples
+#include "tuple/apply.hpp"
+#include "tuple/front.hpp"
+#include "tuple/back.hpp"
+#include "tuple/push_back.hpp"
+#include "tuple/push_front.hpp"
+#include "tuple/pop_back.hpp"
+#include "tuple/pop_front.hpp"
+
+// control
+#include "if.hpp"
+
+// string
+#include "string/char.hpp"
+#include "string/string.hpp"
+#include "string/strcat.hpp"
+
+// algebra
+#include "integral.hpp"
 
 #endif // MPL14_HPP_INCLUDED
