9cd90fffe1575bee18d4397963fc0fa6b4e8687a
5cript <Tim06TR@gmail.com>
2015-06-21 05:55:25 +0200

    fixed linear_congruental_engine::next. Now longer generates 2 numbers.

--- random/linear_congruential_engine.hpp
+++ random/linear_congruential_engine.hpp
@@ -7,7 +7,6 @@ namespace mplex
 {
     namespace random
     {
-
         struct lcg_params
         {
             constexpr static long long m = 2147483647; // 2'147'483'647
@@ -20,17 +19,14 @@ namespace mplex
         {
             constexpr static long long value = (lcg_params::a * previous + lcg_params::c) % lcg_params::m;
 
-            struct next
-            {
-                constexpr static long long value = linear_congruential_engine <value>::value;
-                using type = linear_congruential_engine <value>;
-            };
+            struct next : public linear_congruential_engine <value>
+            { };
         };
 
         struct make_time_seed
         {
             constexpr static long long value =
-                    time::hour * 60 * 60 + time::hour * 60 + time::second + date::day * 60 * 60 * 24;
+                time::hour * 60 * 60 + time::hour * 60 + time::second + date::day * 60 * 60 * 24;
         };
     }
 }
