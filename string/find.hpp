9e4c9426b98de2f6c03ed3c218c74ba11fd04896
5cript <Tim06TR@gmail.com>
2015-06-21 04:21:47 +0200

    Algorithm now fully works and short-circuits the recursion on find

--- string/find.hpp
+++ string/find.hpp
@@ -12,28 +12,30 @@
 
 namespace mplex
 {
-    template <typename String, typename Char, String::size_type Position = 0, bool AtEnd = false>
+    template <typename String, typename Char, typename String::size_type Position = 0, bool AtEnd = false>
     struct find_char_impl
     {
+        constexpr static const bool found = String::template at <Position>::type::value == Char::value;
+
         using type =
-        if_t <std::is_same <typename String::template at <Position>::type, Char>::value,
-                integral <typename String::size_type, Position>,
-                find_char_impl <String, Char, Position + 1, String::size == Position + 1> >;
+        if_t <found,
+              integral <typename String::size_type, Position>,
+              typename find_char_impl <String, Char, Position + 1, String::size == Position + 1 || found>::type >;
     };
 
-    template <typename String, typename Char, String::size_type Position>
+    template <typename String, typename Char, typename String::size_type Position>
     struct find_char_impl <String, Char, Position, true>
     {
         using type = integral <typename String::size_type, String::npos>;
     };
 
-    template <typename String, typename Char, String::size_type StartingPosition>
+    template <typename String, typename Char, typename String::size_type StartingPosition = 0>
     struct find_char
     {
-        using type = find_char <String, Char, StartingPosition, StartingPosition < String::size>::type;
+        using type = typename find_char_impl <String, Char, StartingPosition, !(StartingPosition < String::size)>::type;
     };
 
-    template <typename String, typename Char, String::size_type StartingPosition>
+    template <typename String, typename Char, typename String::size_type StartingPosition = 0>
     using find_char_t = typename find_char <String, Char, StartingPosition>::type;
 
 }
