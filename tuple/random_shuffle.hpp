db029fd4ea169d8971a0f0297c4abc8ebf7b4eea
5cript <Tim06TR@gmail.com>
2015-06-21 05:54:52 +0200

    [WIP] started work on a random shuffle algorithm

---
+++ tuple/random_shuffle.hpp
@@ -0,0 +1,45 @@
+#ifndef RANDOM_SHUFFLE_HPP_INCLUDED
+#define RANDOM_SHUFFLE_HPP_INCLUDED
+
+#include "../random/linear_congruential_engine.hpp"
+#include "../random/uniform_int_distribution.hpp"
+#include "../control/if.hpp"
+#include "element_swap.hpp"
+
+#include <tuple>
+
+/**
+ *  for i = size-1; i != 1; --1
+ *      j = random(0, i)
+ *      swap(a[j], a[i])
+ */
+
+namespace mplex
+{
+    template <typename Tuple, typename RandomEngine, typename Result = std::tuple<>>
+    struct random_shuffle { };
+
+    template <typename RandomEngine, typename Result, typename T, typename ... List>
+    struct random_shuffle <std::tuple<T, List...>, RandomEngine, Result>
+    {
+        enum {
+            rand = random::uniform_int_distribution <long long, RandomEngine::value, 0, sizeof...(List)-1>::value
+        };
+
+        using type =
+        typename random_shuffle <std::tuple <List...>,
+                                 typename RandomEngine::next,
+                                 element_swap_t <std::tuple <T, List...>, rand, sizeof...(List)-1>>::type;
+    };
+
+    template <typename RandomEngine, typename Result>
+    struct random_shuffle <std::tuple<>, RandomEngine, Result>
+    {
+        using type = Result;
+    };
+
+    template <typename Tuple, typename RandomEngine>
+    using random_shuffle_t = typename random_shuffle <Tuple, RandomEngine>::type;
+}
+
+#endif // RANDOM_SHUFFLE_HPP_INCLUDED
