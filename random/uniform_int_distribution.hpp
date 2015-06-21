9cdf477c084664508f277f730faa6c3aa55380ac
5cript <Tim06TR@gmail.com>
2015-06-21 05:56:07 +0200

    Added uniform int distribution for random numbers.

---
+++ random/uniform_int_distribution.hpp
@@ -0,0 +1,16 @@
+#ifndef UNIFORM_INT_DISTRIBUTION_HPP_INCLUDED
+#define UNIFORM_INT_DISTRIBUTION_HPP_INCLUDED
+
+// this is not really uniform! See modulo bias.
+
+namespace mplex { namespace random {
+
+    template <typename T, T Rand, T Max, T Min>
+    struct uniform_int_distribution
+    {
+        constexpr static const T value = (Rand % (Max - Min + 1) + Min);
+    };
+}
+}
+
+#endif // UNIFORM_INT_DISTRIBUTION_HPP_INCLUDED
