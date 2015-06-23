c0f43d36ae0a60b0c0e5f836cef0a543cf00cbcc
5cript <Tim06TR@gmail.com>
2015-06-24 01:46:13 +0200

    Added trimming feature for tuples.

---
+++ string/trim.hpp
@@ -0,0 +1,15 @@
+#ifndef TRIM_HPP_INCLUDED
+#define TRIM_HPP_INCLUDED
+
+#include "../control/if.hpp"
+#include "string.hpp"
+#include "char.hpp"
+#include "char_traits.hpp"
+
+namespace mplex { namespace string_algorithm
+{
+
+}
+}
+
+#endif // TRIM_HPP_INCLUDED
