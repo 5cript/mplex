df17322823fcc70c217f4b7df351fbc5bb741964
5cript <Tim06TR@gmail.com>
2015-06-24 03:08:19 +0200

    Changed include guards.

--- random/random.hpp
+++ random/random.hpp
@@ -1,6 +1,6 @@
-#ifndef RANDOM_HPP_INCLUDED
-#define RANDOM_HPP_INCLUDED
+#ifndef MPL14_RANDOM_HPP_INCLUDED
+#define MPL14_RANDOM_HPP_INCLUDED
 
 #include "linear_congruential_engine.hpp"
 
-#endif // RANDOM_HPP_INCLUDED
+#endif // MPL14_RANDOM_HPP_INCLUDED
