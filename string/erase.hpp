e1e6cfd70d6c07ddd9c592ea155a7252baa5bcfa
5cript <Tim06TR@gmail.com>
2015-06-24 03:25:42 +0200

    [WIP] Erase functions are planned.

---
+++ string/erase.hpp
@@ -0,0 +1,13 @@
+#ifndef ERASE_HPP_INCLUDED
+#define ERASE_HPP_INCLUDED
+
+namespace mplex { namespace string_algorithm
+{
+    template <typename String, typename Range>
+    struct erase {
+
+    };
+}
+}
+
+#endif // ERASE_HPP_INCLUDED
