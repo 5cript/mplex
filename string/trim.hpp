531d0e400123bcab6b2b5018af696e98e32bf11e
5cript <Tim06TR@gmail.com>
2015-06-24 03:06:38 +0200

    String trimming added

--- string/trim.hpp
+++ string/trim.hpp
@@ -1,15 +1,34 @@
-#ifndef TRIM_HPP_INCLUDED
-#define TRIM_HPP_INCLUDED
+#ifndef MPL14_STRING_TRIM_HPP_INCLUDED
+#define MPL14_STRING_TRIM_HPP_INCLUDED
 
-#include "../control/if.hpp"
 #include "string.hpp"
-#include "char.hpp"
-#include "char_traits.hpp"
+#include "../tuple/trim.hpp"
+#include "classification.hpp"
+#include "algorithm_adapter.hpp"
 
 namespace mplex { namespace string_algorithm
 {
+    template <typename String, template <typename> class Predicate = is_space>
+    struct trim_left : adapt <String, mplex::trim_left_t, Predicate>
+    { };
 
+    template <typename String, template <typename> class Predicate = is_space>
+    struct trim_right : adapt <String, mplex::trim_right_t, Predicate>
+    { };
+
+    template <typename String, template <typename> class Predicate = is_space>
+    struct trim : adapt <String, mplex::trim_t, Predicate>
+    { };
+
+    template <typename String, template <typename> class Predicate = is_space>
+    using trim_left_t = typename trim_left <String, Predicate>::type;
+
+    template <typename String, template <typename> class Predicate = is_space>
+    using trim_right_t = typename trim_right <String, Predicate>::type;
+
+    template <typename String, template <typename> class Predicate = is_space>
+    using trim_t = typename string_algorithm::trim <String, Predicate>::type;
 }
 }
 
-#endif // TRIM_HPP_INCLUDED
+#endif // MPL14_STRING_HPP_INCLUDED
