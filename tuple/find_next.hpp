906c7156ec7e55bdbe6ce45d25a1f315172134e1
5cript <Tim06TR@gmail.com>
2015-06-27 01:33:32 +0200

    [WIP] find_next adds to find, which can only find the first match.

---
+++ tuple/find_next.hpp
@@ -0,0 +1,13 @@
+#ifndef MPL14_TUPLE_FIND_NEXT_HPP_INCLUDED
+#define MPL14_TUPLE_FIND_NEXT_HPP_INCLUDED
+
+#include "find.hpp"
+
+namespace mplex {
+    template <typename Tuple, typename What, typename LastMatch, template <typename, typename> class ComparisonPredicate = std::is_same>
+    struct find_next {
+
+    };
+}
+
+#endif // FIND_NEXT_HPP_INCLUDED
