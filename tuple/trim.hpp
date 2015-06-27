d8fec6604d76df0c137ab83bbe0574bb230d2aa4
5cript <Tim06TR@gmail.com>
2015-06-27 22:01:01 +0200

    changed if_t to if_vt

--- tuple/trim.hpp
+++ tuple/trim.hpp
@@ -11,9 +11,9 @@ namespace mplex {
 
     template <typename Predicate, typename T, typename... List>
     struct trim_left <std::tuple <T, List...>, Predicate, false> {
-        using type = if_t <Predicate::template apply <T>::value,
-                           typename trim_left <std::tuple<List...>, Predicate, !Predicate::template apply<T>::value>::type,
-                           std::tuple <T, List...>>;
+        using type = if_vt <Predicate::template apply <T>::value,
+                            typename trim_left <std::tuple<List...>, Predicate, !Predicate::template apply<T>::value>::type,
+                            std::tuple <T, List...>>;
     };
 
     template <typename Predicate>
@@ -33,9 +33,9 @@ namespace mplex {
     template <typename Tuple, typename Predicate, bool Abort = false>
     struct trim_right {
         // Abort == false
-        using type = if_t <Predicate::template apply <typename std::tuple_element <std::tuple_size <Tuple>::value - 1, Tuple>::type>::value,
-                           typename trim_right <pop_back_t<Tuple>, Predicate>::type,
-                           Tuple>;
+        using type = if_vt <Predicate::template apply <typename std::tuple_element <std::tuple_size <Tuple>::value - 1, Tuple>::type>::value,
+                            typename trim_right <pop_back_t<Tuple>, Predicate>::type,
+                            Tuple>;
     };
 
     template <typename Tuple, typename Predicate>
