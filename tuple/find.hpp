6f53156d263f1abe75c655a03ef67ad1ee9ae47a
5cript <Tim06TR@gmail.com>
2015-06-27 22:03:20 +0200

    Replaced if_t with Changed if_t to if_vt.

--- tuple/find.hpp
+++ tuple/find.hpp
@@ -22,21 +22,21 @@ namespace mplex {
         constexpr static const bool char_match = ComparisonPredicate::template apply <T, typename std::tuple_element <SWPos::value, What>::type >::value;
 
         // where is the match
-        using next_where = if_t <char_match,
-                                 if_t <Where::value == -1,
-                                       Pos,
-                                       Where>,
-                                 int_<-1>>;
+        using next_where = if_vt <char_match,
+                                  if_vt <Where::value == -1,
+                                         Pos,
+                                         Where>,
+                                  int_<-1>>;
 
         // next index in the search word
-        using next_sw = if_t <char_match,
-                              int_<SWPos::value + 1>,
-                              int_<0>>;
+        using next_sw = if_vt <char_match,
+                               int_<SWPos::value + 1>,
+                               int_<0>>;
 
         // next tuple to be inspected [short circuited]
-        using next_tuple = if_t <SWPos::value == std::tuple_size <What>::value - 1,
-                                 std::tuple<>,
-                                 std::tuple<List...>> ;
+        using next_tuple = if_vt <SWPos::value == std::tuple_size <What>::value - 1,
+                                  std::tuple<>,
+                                  std::tuple<List...>> ;
 
         using type = typename find <next_tuple, What, ComparisonPredicate, next_where, int_<Pos::value + 1>, next_sw>::type;
     };
@@ -49,7 +49,7 @@ namespace mplex {
 
         using type = struct this_type {
             constexpr static const bool found = SWPos::value == std::tuple_size <What>::value;
-            constexpr static const int where = if_t <found, Where, int_<-1>>::value;
+            constexpr static const int where = if_vt <found, Where, int_<-1>>::value;
             using where_t = int_<where>;
             constexpr static const int value = where;
         };
