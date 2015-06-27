61cabe2d5aa15fac06e3c2d6750fed13e0763cc0
5cript <Tim06TR@gmail.com>
2015-06-27 02:55:01 +0200

    Added string erase method.

--- string/erase.hpp
+++ string/erase.hpp
@@ -1,13 +1,23 @@
-#ifndef ERASE_HPP_INCLUDED
-#define ERASE_HPP_INCLUDED
+#ifndef MPL14_STRING_ERASE_HPP_INCLUDED
+#define MPL14_STRING_ERASE_HPP_INCLUDED
+
+#include "string.hpp"
+#include "../tuple/apply.hpp"
+#include "../tuple/erase.hpp"
 
 namespace mplex { namespace string_algorithm
 {
-    template <typename String, typename Range>
+    template <typename String, typename Begin, typename End>
     struct erase {
-
+        using type = apply_t <mplex::erase_t <typename String::type, Begin, End>, mplex::translate>;
     };
+
+    template <typename String, typename Begin, typename End>
+    using erase_t = typename erase <String, Begin, End>::type;
+
+    template <typename String, unsigned Begin, unsigned End>
+    using erase_vt = typename erase <String, unsigned_<Begin>, unsigned_<End>>::type;
 }
 }
 
-#endif // ERASE_HPP_INCLUDED
+#endif // MPL14_STRING_ERASE_HPP_INCLUDED
