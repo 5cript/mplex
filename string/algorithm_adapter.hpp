faa4732fc656af7ab6da363c32f07cdb5b12f33d
5cript <Tim06TR@gmail.com>
2015-06-27 18:27:08 +0200

    Can now also adapt algorithms performing on strings.

--- string/algorithm_adapter.hpp
+++ string/algorithm_adapter.hpp
@@ -10,6 +10,15 @@ namespace mplex {
     struct adapt {
         using type = apply_t <TupleAlgorithm <typename String::type, AdditionalParameters...>, mplex::translate>;
     };
+
+    template <typename String, template <typename, typename...> class TupleAlgorithm, typename... AdditionalParameters>
+    struct adapt_list {
+
+        template <typename... Tuples>
+        using list_apply = std::tuple <apply_t <Tuples, mplex::translate>...>;
+
+        using type = apply_t <TupleAlgorithm <typename String::type, AdditionalParameters...>, list_apply>;
+    };
 }
 
 #endif // MPL14_STRING_ALGORITHM_ADAPTER_HPP_INCLUDED
