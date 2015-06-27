5a4d63d65e25a99ad1fa8784b726ccc5ecec39f0
5cript <Tim06TR@gmail.com>
2015-06-27 19:54:47 +0200

    Added compiler detection via macros. Used in type_print.
    This does not mean that more compiler support will come.
    Currently only supporting g++, but clang support is planned.

--- config.hpp
+++ config.hpp
@@ -3,4 +3,19 @@
 
 #define UTF_CHAR
 
+#ifdef __clang__
+#   define CLANG
+#elif defined(__ICC)
+#   define INTEL
+#   warning "This library is most likely not supported by this compiler"
+#elif defined(__GNUC__)
+#   define GCC
+#elif defined(_MSC_VER)
+#   define VCPP
+#   error "This library is not supported by the microsoft compiler"
+#else
+#   error "Unknown compiler"
+#endif
+
+
 #endif // CONFIG_HPP_INCLUDED
