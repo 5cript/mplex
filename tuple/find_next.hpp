f3fdfe75bdd1a13bf66edafec939a2c876535fa4
5cript <Tim06TR@gmail.com>
2015-06-28 23:36:13 +0200

    Fixed find_next. Default predicate now is a functor.

--- tuple/find_next.hpp
+++ tuple/find_next.hpp
@@ -3,14 +3,15 @@
 
 #include "find.hpp"
 #include "erase.hpp"
+#include "../fundamental/is_same.hpp"
 
 namespace mplex {
-    template <typename Tuple, typename What, typename LastMatch, typename ComparisonPredicate = std::is_same>
+    template <typename Tuple, typename What, typename LastMatch, typename ComparisonPredicate = is_same>
     struct find_next {
         using type = find_t <erase_t <Tuple, unsigned_<0>, LastMatch>, What, ComparisonPredicate>;
     };
 
-    template <typename Tuple, typename What, typename LastMatch, typename ComparisonPredicate = std::is_same>
+    template <typename Tuple, typename What, typename LastMatch, typename ComparisonPredicate = is_same>
     using find_next_t = typename find_next <Tuple, What, LastMatch, ComparisonPredicate>::type;
 }
 
