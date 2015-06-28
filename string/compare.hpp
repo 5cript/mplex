860b743b0a11ef6ef428b565213dbb81f418a3d8
5cript <Tim06TR@gmail.com>
2015-06-28 05:30:32 +0200

    Added string comparison functor. All other functors in this file got an additional type member which is an integral wrapper around value.

--- string/compare.hpp
+++ string/compare.hpp
@@ -2,12 +2,22 @@
 #define MPL14_STRING_COMPARE_HPP_INCLUDED
 
 #include "char.hpp"
+#include "string.hpp"
+
+#include "../tuple/pop_front.hpp"
+#include "../tuple/front.hpp"
+
+#include "../fundamental/eval_if.hpp"
+#include "../fundamental/integral.hpp"
+
+#include <type_traits>
 
 namespace mplex {
     struct is_equal {
         template <typename CharT, typename CharU>
         struct apply {
             constexpr static const bool value = CharT::value == CharU::value;
+            using type = bool_ <value>;
         };
     };
 
@@ -15,6 +25,7 @@ namespace mplex {
         template <typename CharT, typename CharU>
         struct apply {
             constexpr static const bool value = to_lower::template apply <CharT>::type::value == to_lower::template apply <CharU>::type::value;
+            using type = bool_ <value>;
         };
     };
 
@@ -22,6 +33,7 @@ namespace mplex {
         template <typename CharT, typename CharU>
         struct apply {
             constexpr static const bool value = CharT::value < CharU::value;
+            using type = bool_ <value>;
         };
     };
 
@@ -29,6 +41,7 @@ namespace mplex {
         template <typename CharT, typename CharU>
         struct apply {
             constexpr static const bool value = to_lower::template apply <CharT>::type::value < to_lower::template apply <CharU>::type::value;
+            using type = bool_ <value>;
         };
     };
 
@@ -36,6 +49,7 @@ namespace mplex {
         template <typename CharT, typename CharU>
         struct apply {
             constexpr static const bool value = !(CharT::value > CharU::value);
+            using type = bool_ <value>;
         };
     };
 
@@ -43,6 +57,44 @@ namespace mplex {
         template <typename CharT, typename CharU>
         struct apply {
             constexpr static const bool value = !(to_lower::template apply <CharT>::type::value > to_lower::template apply <CharU>::type::value);
+            using type = bool_ <value>;
+        };
+    };
+
+    namespace internal {
+        template <typename StringT, typename StringU, typename Predicate>
+        struct streq {
+            using lhsFront = eval_if_default_t <bool_ <!StringT::empty::value>, char_<'.'>, front_a, typename StringT::type>;
+            using rhsFront = eval_if_default_t <bool_ <!StringT::empty::value>, char_<'.'>, front_a, typename StringU::type>;
+
+            using step = typename Predicate::template apply <lhsFront, rhsFront>::type;
+
+            constexpr static const bool value =
+                step::value && streq <typename StringT::pop_front_t, typename StringU::pop_front_t, Predicate>::value;
+        };
+
+        template <typename Predicate>
+        struct streq <mplex::string<>, mplex::string<>, Predicate> {
+            constexpr static const bool value = true;
+        };
+
+        template <typename String, typename Predicate>
+        struct streq <String, mplex::string<>, Predicate> {
+            constexpr static const bool value = false;
+        };
+
+        template <typename String, typename Predicate>
+        struct streq <mplex::string<>, String, Predicate> {
+            constexpr static const bool value = false;
+        };
+    }
+
+    template <typename Predicate>
+    struct equals {
+        template <typename StringT, typename StringU>
+        struct apply {
+            constexpr static const bool value = internal::streq <StringT, StringU, Predicate>::value;
+            using type = bool_ <value>;
         };
     };
 }
