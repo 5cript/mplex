932fbcf3ce2488477e67bfba876ceeb47b01a7d1
5cript <Tim06TR@gmail.com>
2015-06-28 05:29:41 +0200

    Added lazy evaluation wrapper for functors.

---
+++ fundamental/eval_if.hpp
@@ -0,0 +1,37 @@
+#ifndef MPL14_FUNDAMENTAL_EVAL_IF_HPP_INCLUDED
+#define MPL14_FUNDAMENTAL_EVAL_IF_HPP_INCLUDED
+
+namespace mplex {
+
+    template <typename Condition, typename Fallback, typename Function, typename... Parameters>
+    struct eval_if_default
+    {
+        using type = Fallback; // TODO: think about something more meaningfull here
+    };
+
+    template <typename Fallback, typename Function, typename... Parameters>
+    struct eval_if_default <true_, Fallback, Function, Parameters...>
+    {
+        using type = typename Function::template apply <Parameters...>::type;
+    };
+
+    template <typename Condition, typename Function, typename... Parameters>
+    struct eval_if
+    {
+        using type = void; // TODO: think about something more meaningfull here
+    };
+
+    template <typename Function, typename... Parameters>
+    struct eval_if <true_, Function, Parameters...>
+    {
+        using type = typename Function::template apply <Parameters...>::type;
+    };
+
+    template <typename Condition, typename Function, typename... Parameters>
+    using eval_if_t = typename eval_if <Condition, Function, Parameters...>::type;
+
+    template <typename Condition, typename Fallback, typename Function, typename... Parameters>
+    using eval_if_default_t = typename eval_if_default <Condition, Fallback, Function, Parameters...>::type;
+}
+
+#endif // MPL14_FUNDAMENTAL_EVAL_IF_HPP_INCLUDED
