aa09b78030df29770389332f4bac7f4bd4dfb0a4
5cript <Tim06TR@gmail.com>
2015-07-04 23:09:32 +0200

    Added utility to determine functor arity.

--- functional/functor.hpp
+++ functional/functor.hpp
@@ -1,6 +1,10 @@
 #ifndef MPL14_FUNCTIONAL_FUNCTOR_HPP_INCLUDED
 #define MPL14_FUNCTIONAL_FUNCTOR_HPP_INCLUDED
 
+#include "../tuple/apply.hpp"
+#include "../tuple/make_tuple.hpp"
+#include "../tuple/push_front.hpp"
+
 /**
  *  A functor is defined as class that containing an apply template
  *
@@ -8,28 +12,32 @@
  */
 
 namespace mplex {
-    /*
-    struct _level_1 {
-        template <typename T>
-        apply {
-            using type = T;
+    namespace internal {
+        template <typename T, typename... List>
+        struct has_arity_impl {
+            typedef char yes[1];
+            typedef char no[2];
+
+            template <typename C>
+            static yes& test(typename C::template apply<List...>*);
+
+            template <typename>
+            static no& test(...);
+
+            using type = bool_ <sizeof(test<T>(nullptr)) == sizeof(yes)>;
+            constexpr static const bool value = type::value;
         };
+    }
+
+    template <typename T, typename Arity>
+    struct has_arity {
+        using temp_tuple = push_front_t <typename make_tuple <generators::nulls>::template apply <Arity>::type, T>;
+        using type = bool_ <apply_t <temp_tuple, internal::has_arity_impl>::value>;
+        constexpr static const bool value = type::value;
     };
 
-    struct _level_2 {
-        template <typename T, typename U>
-        apply {
-            using type = _binder<T, U>;
-        };
-    };
-
-    struct _level_many {
-        template <typename... T>
-        apply {
-            using type = _many_binder<T...>;
-        };
-    };
-    */
+    template <typename T, unsigned Arity>
+    using has_arity_v = typename has_arity <T, unsigned_ <Arity> >::type;
 }
 
 #endif // MPL14_FUNCTIONAL_FUNCTOR_HPP_INCLUDED
