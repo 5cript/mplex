7c805bfbfcd9f29e52ca2692dd5a9ad0986d6faa
5cript <Tim06TR@gmail.com>
2015-07-22 23:40:31 +0200

    Moved bind feature that got developed here to bind. Beginning work on this anew.

--- functional/compose.hpp
+++ functional/compose.hpp
@@ -1,55 +1,23 @@
 #ifndef MPL14_FUNCTIONAL_COMPOSE_HPP_INCLUDED
 #define MPL14_FUNCTIONAL_COMPOSE_HPP_INCLUDED
 
+#include "../fundamental/identity.hpp"
+
+#include "../control/if.hpp"
+
+#include "../tuple/tuple_element.hpp"
+#include "../tuple/push_back.hpp"
+#include "../tuple/push_front.hpp"
+#include "../tuple/make_tuple.hpp"
+#include "../tuple/apply.hpp"
+
+#include "../algorithm/merge.hpp"
+#include "../algorithm/count.hpp"
+
 #include <tuple>
 
 namespace mplex {
-    namespace placeholders {
-        template <unsigned V>
-        struct _ {
-            constexpr static const unsigned value = V;
-        };
 
-        #define PLACEHOLDER_SHORT_HAND(NUM) \
-        using _ ## NUM = _<NUM>
-
-        PLACEHOLDER_SHORT_HAND(1);
-        PLACEHOLDER_SHORT_HAND(2);
-        PLACEHOLDER_SHORT_HAND(3);
-        PLACEHOLDER_SHORT_HAND(4);
-        PLACEHOLDER_SHORT_HAND(5);
-        PLACEHOLDER_SHORT_HAND(6);
-        PLACEHOLDER_SHORT_HAND(7);
-        PLACEHOLDER_SHORT_HAND(8);
-        PLACEHOLDER_SHORT_HAND(9);
-        PLACEHOLDER_SHORT_HAND(10);
-        PLACEHOLDER_SHORT_HAND(11);
-        PLACEHOLDER_SHORT_HAND(12);
-        PLACEHOLDER_SHORT_HAND(13);
-        PLACEHOLDER_SHORT_HAND(14);
-        PLACEHOLDER_SHORT_HAND(15);
-        PLACEHOLDER_SHORT_HAND(16);
-        PLACEHOLDER_SHORT_HAND(17);
-        PLACEHOLDER_SHORT_HAND(18);
-        PLACEHOLDER_SHORT_HAND(19);
-        PLACEHOLDER_SHORT_HAND(20);
-    }
-
-    /*
-    using Add = make_delegate <add, _1, _2>;
-    */
-
-    template <typename Functor, typename... Parameters>
-    struct make_delegate
-    {
-        template <typename... InnerParameters>
-        struct apply
-        {
-            using _temp = std::tuple <InnerParameters...>;
-            static_assert (sizeof...(InnerParameters) == sizeof...(Parameters), "Incorrect amount of arguments given.");
-            using type = typename Functor::template apply <typename std::tuple_element <Parameters::value, _temp>::type...>::type;
-        };
-    };
 }
 
 #endif // MPL14_FUNCTIONAL_COMPOSE_HPP_INCLUDED
