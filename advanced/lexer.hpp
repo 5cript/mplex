3a258e3a0e1975147eec19b39afb91ff7e4e86ae
5cript <Tim06TR@gmail.com>
2015-07-22 23:39:18 +0200

    Fixed includes of moved files.

--- advanced/lexer.hpp
+++ advanced/lexer.hpp
@@ -1,12 +1,12 @@
 #ifndef MPL14_ADVANCED_LEXER_HPP_INCLUDED
 #define MPL14_ADVANCED_LEXER_HPP_INCLUDED
 
-#include "../tuple/remove_if.hpp"
+#include "../algorithm/remove_if.hpp"
 
 namespace mplex {
     template <typename String>
     struct make_lexemes {
-        using is_empty_ = mplex::bind <std::is_same, std::tuple<>>;
+        using is_empty_ = mplex::bind_left <std::is_same, std::tuple<>>;
         using type = remove_if_t <string_algorithm::split_t <String, is_space>, is_empty_>;
     };
 
