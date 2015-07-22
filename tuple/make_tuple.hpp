70917830eb1dfc41b34b268d4154854bd695d079
5cript <Tim06TR@gmail.com>
2015-07-22 23:38:48 +0200

    Generators are now Functors and not Functions.

--- tuple/make_tuple.hpp
+++ tuple/make_tuple.hpp
@@ -7,31 +7,36 @@
 
 namespace mplex {
     namespace generators {
-        template <unsigned I>
         struct nulls {
-            using type = null_t;
+            template <unsigned I>
+            struct apply {
+                using type = null_t;
+            };
         };
 
-        template <unsigned I>
-        struct indices {
-            using type = unsigned_ <I>;
+        struct indices
+        {
+            template <unsigned I>
+            struct apply {
+                using type = unsigned_ <I>;
+            };
         };
     }
 
     namespace internal {
-        template <template <unsigned> class Generator, typename Index, typename Length>
+        template <typename Generator, typename Index, typename Length>
         struct make_tuple_impl {
-            using type = concat_t <std::tuple <typename Generator <Length::value - Index::value>::type>,
+            using type = concat_t <std::tuple <typename Generator::template apply <Length::value - Index::value>::type>,
                                    typename make_tuple_impl <Generator, unsigned_ <Index::value - 1>, Length>::type >;
         };
 
-        template <template <unsigned> class Generator, typename Length>
+        template <typename Generator, typename Length>
         struct make_tuple_impl <Generator, unsigned_ <0>, Length> {
             using type = std::tuple <>;
         };
     }
 
-    template <template <unsigned> class Generator = generators::indices>
+    template <typename Generator = generators::indices>
     struct make_tuple {
         template <typename Length>
         struct apply {
